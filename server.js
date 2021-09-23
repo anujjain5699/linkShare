57e3281473e62385f2a797b9ec368181f27126a3
anuj jain <anujjian5699@gmail.com>
2021-09-23 23:34:38 +0530

    edit server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(cors(corsOption))
 app.use(function(req, res,next){
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin,X-requested-With,Content-Type,Accept")
+    next()
 })
 
 //connect database
