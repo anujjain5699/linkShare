bf455772eda7c8a9ce7715f959aaced626bd7802
anuj jain <anujjian5699@gmail.com>
2021-09-24 17:26:23 +0530

    edit

--- server.js
+++ server.js
@@ -16,11 +16,12 @@ app.use(express.json());
 app.set('view engine','ejs')
 app.use(express.static('public'))
 app.use(cors(corsOption))
-app.use(function(req, res,next){
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin,X-requested-With,Content-Type,Accept")
-    next()
-})
+// app.use(function(req, res,next){
+//     res.header("Access-Control-Allow-Origin", "*")
+//     res.header("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
+//     res.header("Access-Control-Allow-Headers", "Origin,X-requested-With,Content-Type,Accept")
+//     next()
+// })
 
 //connect database
 connectDB()
--- utils/js/index.js
+++ utils/js/index.js
@@ -6,7 +6,7 @@ const bgProgress = document.querySelector(".bg-progress");
 const progressPercent = document.querySelector("#progressPercent");
 const progressContainer = document.querySelector(".progress-container");
 const progressBar = document.querySelector(".progress-bar");
-const status = document.querySelector(".status");
+const uploadingStatus = document.querySelector(".uploadingStatus");
 
 const sharingContainer = document.querySelector(".sharing-container");
 const copyURLBtn = document.querySelector("#copyURLBtn");
@@ -30,22 +30,28 @@ dropZone.addEventListener("drop", (e) => {
   e.preventDefault();
   //   console.log("dropped", e.dataTransfer.files[0].name);
   const files = e.dataTransfer.files;
-  if (files.length === 1) {
-    if (files[0].size < maxAllowedSize) {
-      fileInput.files = files;
-      uploadFile();
-    } else {
-      showToast("Max file size is 100MB");
-    }
-  } else if (files.length > 1) {
-    showToast("You can't upload multiple files");
+  // if (files.length === 1) {
+  //   if (files[0].size < maxAllowedSize) {
+  //     fileInput.files = files;
+  //     uploadFile();
+  //   } else {
+  //     showToast("Max file size is 100MB");
+  //   }
+  // } else if (files.length > 1) {
+  //   showToast("You can't upload multiple files");
+  // }
+  if(files.length){
+    fileInput.files=files;
+    uploadFile()
   }
   dropZone.classList.remove("dragged");
 });
 
 dropZone.addEventListener("dragover", (e) => {
   e.preventDefault();
-  dropZone.classList.add("dragged");
+  if(!dropZone.classList.contains("dragged")) {
+    dropZone.classList.add("dragged");
+  }
 
   // console.log("dropping file");
 });
@@ -80,36 +86,47 @@ fileURL.addEventListener("click", () => {
 const uploadFile = () => {
   console.log("file added uploading");
 
-  files = fileInput.files;
+  const file = fileInput.files[0];
   const formData = new FormData();
-  formData.append("myFile", files[0]);
+  formData.append("myfile", file);
+ // console.log(file);
+  console.log(formData)
 
-  //show the uploader
-  progressContainer.style.display = "block";
+  // //show the uploader
+  // progressContainer.style.display = "block";
 
   // upload file
   const xhr = new XMLHttpRequest();
 
-  // listen for upload progress
-  xhr.upload.onprogress = function (event) {
-    // find the percentage of uploaded
-    let percent = Math.round((100 * event.loaded) / event.total);
-    progressPercent.innerText = percent;
-    const scaleX = `scaleX(${percent / 100})`;
-    bgProgress.style.transform = scaleX;
-    progressBar.style.transform = scaleX;
-  };
+  // // listen for upload progress
+  // xhr.upload.onprogress = function (event) {
+  //   // find the percentage of uploaded
+  //   let percent = Math.round((100 * event.loaded) / event.total);
+  //   progressPercent.innerText = percent;
+  //   const scaleX = `scaleX(${percent / 100})`;
+  //   bgProgress.style.transform = scaleX;
+  //   progressBar.style.transform = scaleX;
+  // };
 
-  // handle error
-  xhr.upload.onerror = function () {
-    fileInput.value = ""; // reset the input
-    showToast(`Error in upload: ${xhr.statusText}.`);
-  };
+  // // handle error
+  // xhr.upload.onerror = function () {
+  //   fileInput.value = ""; // reset the input
+  //   showToast(`Error in upload: ${xhr.statusText}.`);
+  // };
 
   // listen for response which will give the link
   xhr.onreadystatechange = function () {
     if (xhr.readyState == XMLHttpRequest.DONE) {
-      onFileUploadSuccess(xhr.responseText);
+      //onFileUploadSuccess(xhr.response);
+      //console.log(xhr.responseText)
+      var status = xhr.status;
+      if (status === 0 || (status >= 200 && status < 400)) {
+        // The request has been completed successfully
+        console.log(xhr.responseText);
+      } else {
+        // Oh no! There has been an error with the request!
+        console.log("xhr request error")
+      }
     }
   };
 
@@ -119,7 +136,7 @@ const uploadFile = () => {
 
 const onFileUploadSuccess = (res) => {
   fileInput.value = ""; // reset the input
-  status.innerText = "Uploaded";
+  uploadingStatus.innerText = "Uploaded";
 
   // remove the disabled attribute from form btn & make text send
   emailForm[2].removeAttribute("disabled");
